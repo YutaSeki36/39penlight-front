7a6a8f0bff650807b34d6f77546421115a132f69
YutaSeki36 <sekiyuta0306@gmail.com>
2021-06-09 21:50:11 +0900

    util追加

---
+++ src/util/APIUtils.tsx
@@ -0,0 +1,32 @@
+import { API_BASE_URL } from "../config";
+
+type PostObject = {
+    color_code: string
+};
+
+const request = (options: any) => {
+    const headers = new Headers({
+      "Content-Type": "application/json"
+    });
+  
+    const defaults = { headers: headers };
+    options = Object.assign({}, defaults, options);
+  
+    return fetch(options.url, options).then(response =>
+      response.json().then(json => {
+        if (!response.ok) {
+          return Promise.reject(json);
+        }
+        return json;
+      })
+    );
+  };
+
+export function changePenlightColor(colorCode: PostObject) {
+    return request({
+      url: API_BASE_URL + "/color",
+      method: "POST",
+      body: JSON.stringify(colorCode)
+    });
+  }
+  
