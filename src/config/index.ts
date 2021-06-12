8f4bbe309fa7040b7f294b5e77f2b255cafab219
YutaSeki36 <sekiyuta0306@gmail.com>
2021-06-12 21:42:12 +0900

    url修正

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://pure-reef-17002.herokuapp.com/"
+export const API_BASE_URL = "https://pure-reef-17002.herokuapp.com"
     // process.env.PENLIGHT_API_BASE_URL || "http://localhost:8080";
