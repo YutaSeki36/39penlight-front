64439975a40ecf4c129128a4e8756f60425c0afd
YutaSeki36 <sekiyuta0306@gmail.com>
2021-06-12 23:21:12 +0900

    アイドル追加

--- src/App.tsx
+++ src/App.tsx
@@ -78,11 +78,131 @@ const initData: Idols = {
     },
     {
       name: "かおり",
-      colorCode: "#ea5b76",
+      colorCode: "#274079",
     },
     {
-      name: "かおり",
-      colorCode: "#ea5b76",
+      name: "みなこ",
+      colorCode: "#58a6dc",
+    },
+    {
+      name: "かれん",
+      colorCode: "#b63b40",
+    },
+    {
+      name: "みなこ",
+      colorCode: "#58a6dc",
+    },
+    {
+      name: "えれな",
+      colorCode: "#9bce92",
+    },
+    {
+      name: "じゅりあ",
+      colorCode: "#d7385f",
+    },
+    {
+      name: "つむぎ",
+      colorCode: "#ebe1ff",
+    },
+    {
+      name: "ももこ",
+      colorCode: "#efb864",
+    },
+    {
+      name: "さよこ",
+      colorCode: "#7f6575",
+    },
+    {
+      name: "ことは",
+      colorCode: "#92cfbb",
+    },
+    {
+      name: "ともか",
+      colorCode: "#bee3e3",
+    },
+    {
+      name: "まつり",
+      colorCode: "#5abfb7",
+    },
+    {
+      name: "めぐみ",
+      colorCode: "#454341",
+    },
+    {
+      name: "ふうか",
+      colorCode: "#7278a8",
+    },
+    {
+      name: "いく",
+      colorCode: "#f7e78e",
+    },
+    {
+      name: "すばる",
+      colorCode: "#aeb49c",
+    },
+    {
+      name: "ゆりこ",
+      colorCode: "#c7b83c",
+    },
+    {
+      name: "ちづる",
+      colorCode: "#f19557",
+    },
+    {
+      name: "あかね",
+      colorCode: "#eb613f",
+    },
+    {
+      name: "せりか",
+      colorCode: "#ed90ba",
+    },
+    {
+      name: "このみ",
+      colorCode: "#f1becb",
+    },
+    {
+      name: "のりこ",
+      colorCode: "#eceb70",
+    },
+    {
+      name: "あゆむ",
+      colorCode: "#e25a9b",
+    },
+    {
+      name: "みずき",
+      colorCode: "#99b7dc",
+    },
+    {
+      name: "ありさ",
+      colorCode: "#b54461",
+    },
+    {
+      name: "みや",
+      colorCode: "#d7a96b",
+    },
+    {
+      name: "しずか",
+      colorCode: "#6495cf",
+    },
+    {
+      name: "あんな",
+      colorCode: "#7e6ca8",
+    },
+    {
+      name: "りお",
+      colorCode: "#f19591",
+    },
+    {
+      name: "かな",
+      colorCode: "#f5ad3b",
+    },
+    {
+      name: "なお",
+      colorCode: "#788bc5",
+    },
+    {
+      name: "ろこ",
+      colorCode: "#fff03c",
     },
   ]
 }
